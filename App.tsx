faec7d37eb659a2f1dbed5abd05fe4dc6396a509
diptags <rd.pradipta@gmail.com>
2025-07-14 10:18:17 +0700

    Add import metro-runtime line to enable fast refresh on web

--- App.tsx
+++ App.tsx
@@ -1,3 +1,5 @@
+import '@expo/metro-runtime';
+
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { createStackNavigator } from '@react-navigation/stack';
