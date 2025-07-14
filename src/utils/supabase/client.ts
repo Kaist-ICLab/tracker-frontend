7d1ae1ac616130541c4683b38d440a50cd10c865
diptags <rd.pradipta@gmail.com>
2025-07-14 10:19:18 +0700

    Add Dotenv and Supabase Package and Config

---
+++ src/utils/supabase/client.ts
@@ -0,0 +1,6 @@
+import { SUPABASE_URL, SUPABASE_KEY } from "@env";
+import { createClient } from "@supabase/supabase-js";
+
+const supabase = createClient(SUPABASE_URL, SUPABASE_KEY);
+
+export default supabase;
