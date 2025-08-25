456a49ce44cfb573c185ff78c20a580c786334ab
diptags <rd.pradipta@gmail.com>
2025-08-25 22:59:54 +0900

    Add simple permission native calling

--- modules/android-tracker-lib/src/AndroidTrackerLibModule.web.ts
+++ modules/android-tracker-lib/src/AndroidTrackerLibModule.web.ts
@@ -1,18 +1,19 @@
 import { registerWebModule, NativeModule } from 'expo';
-
 import { ChangeEventPayload } from './AndroidTrackerLib.types';
 
 type AndroidTrackerLibModuleEvents = {
   onChange: (params: ChangeEventPayload) => void;
 }
 
+// Those functions are not implemented for web, so they will return undefined.
+// See AndroidTrackerLibModule.ts file for implementation on native platforms.
+// Most of the functions will not be implemented for the web version
 class AndroidTrackerLibModule extends NativeModule<AndroidTrackerLibModuleEvents> {
-  PI = Math.PI;
-  async setValueAsync(value: string): Promise<void> {
-    this.emit('onChange', { value });
+  getUserPermissions() {
+    return;
   }
-  hello() {
-    return 'Hello world! 👋';
+  requestPermission() {
+    return;
   }
 };
 
